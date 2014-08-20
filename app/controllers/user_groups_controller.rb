c170947178cd3b6ae3ad06e523ea6fc269e416cc
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-08-20 11:09:34 -0700

    User groups may now be created

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,13 +1,16 @@
 class UserGroupsController < ApplicationController
 
 	def index
+		@groups = UserGroup.all
+		@group = UserGroup.new
 	end
 
 	def create
-		@group = current_user.user_groups.create(group_params)
+		@group = UserGroup.create(group_params)
+		redirect_to '/user_groups', :notice => "Your group has created"
 	end
 
-	def item_params
+	def group_params
 		params.require(:user_group).permit(:group_name)
 	end
 
--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,6 +1,7 @@
 class UserGroup < ActiveRecord::Base
 
 	has_many :user_group_items
+	has_many :users #added - not in original gist
   	has_many :items, through: :user_group_items
   	
 end
