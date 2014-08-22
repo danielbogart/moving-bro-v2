91fbea13d41a9b3d40fefc06bec5ac86071d4724
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-08-21 19:40:33 -0700

    When UG is created a list of all UGI is created

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -5,4 +5,14 @@ class UserGroup < ActiveRecord::Base
   	has_many :items, through: :user_group_items
   	
   	validates :group_name, :presence => true, :uniqueness => true
+
+  	def create_group_items
+  		Item.all.each do |item|
+  			user_group_item = self.user_group_items.build(item_id: item.id, taken: 1)
+  			user_group_item.save
+  		end
+  	end
+
+  	after_create :create_group_items
+  	
 end
