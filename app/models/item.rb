ccad08c9fcc6c4fb2600a70844ac235d6b08c1fa
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-08-27 09:59:37 -0700

    Minor refactoring

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,4 @@ class Item < ActiveRecord::Base
 	has_many :user_group_items
 	has_many :user_groups, through: :user_group_items
 
-
-  #get this working to use instead of if/else statements in view
-  def cleaning_items
-      self.where('category_name = ?', 'Cleaning')
-  end
-
 end
--- app/models/user_group_item.rb
+++ app/models/user_group_item.rb
@@ -3,4 +3,12 @@ class UserGroupItem < ActiveRecord::Base
 	belongs_to :user_group
   	belongs_to :item
 
+  	#works but places blank rows in table on view
+    def cleaning_item
+      if self.item.category_name == "Cleaning"
+      	return self.item.item_name
+      else
+      end
+   	end
+
 end
